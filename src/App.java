09388ea37a61859c6328cd22974ad6bd181562d3
Michel Balvin Ormedo <michelbalvinor@gmail.com>
2024-11-10 19:28:37 -0500

    Delete Proyecto

--- src/App.java
+++
@@ -1,8 +0,0 @@
-import Planteo.AplicacionConsola;
-
-public class App {
-    public static void main(String[] args) throws Exception {
-        AplicacionConsola app = new AplicacionConsola();
-        app.ejecutar();
-    }
-}
